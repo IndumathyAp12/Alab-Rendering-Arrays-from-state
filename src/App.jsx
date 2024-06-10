df3f3b2511f243ad92e8f7b4f04c1c442a22b438
Indu Prabha <apindhuece12@gmail.com>
2024-06-10 18:04:57 -0400

    css is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {React, useState } from 'react';
 // import React from 'react';
 import Learner from '../components/Learner';
-import Score from '../components/Score';
+
 import './App.css';
 
 
